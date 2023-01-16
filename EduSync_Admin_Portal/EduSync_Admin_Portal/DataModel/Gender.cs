7be06b48232ea1714c2de429698061c220215194
cjaybond <jamesuchennachi@gmail.com>
2023-01-16 20:34:10 +0100

    Created DataModels

---
+++ EduSync_Admin_Portal/EduSync_Admin_Portal/DataModel/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EduSync_Admin_Portal.DataModel
+{
+    public class Gender
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
